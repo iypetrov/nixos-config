54fc040646ee6a9647b29e4a1f6873d8cda0f865
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2026-09-11 12:39:37 +0300

    fix: use docker0 bridge gw
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- gardener.nix
+++ gardener.nix
@@ -26,6 +26,6 @@
 
   virtualisation.docker.daemon.settings = {
     insecure-registries = [ "registry.local.gardener.cloud:5001" ];
-    dns = [ "1.1.1.1" "8.8.8.8" ];
+    dns = [ "172.17.0.1" ];
   };
 }
